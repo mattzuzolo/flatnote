7736873b50fda8f5da7f7ada11cd34bb3622d2fe
github username <github email address>
2018-07-04 11:18:12 -0400

    Done.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const sidebar = document.getElementById('sidebar')
 const detail = document.getElementById('detail')
-const deleteButtons = document.getElementsByClassName('detail')
-
 
 document.addEventListener("DOMContentLoaded", function() {
 
@@ -11,13 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(() => console.log("Fetch complete"))
     .then(() => findSidebarNode());
     // .then(() => deletePost());
-
-    document.addEventListener("Click",
-
 });
 
 //add selector just for sidebar before DOM is loaded
-
 function findSidebarNode() {
   const sidebarNodes = document.querySelectorAll("#sidebar div")
   //iterate over node list with for..of
@@ -26,52 +20,34 @@ function findSidebarNode() {
     //The event listener can be specified as either a callback function or as an object that implements EventListener, whose handleEvent() method serves as the callback function.
     //https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#The_event_listener_callback
     sidebarElement.addEventListener("click", handleSidebarNode)
-
-    console.log(sidebarElement);
   }
-
 }
 
 //filter collection of the node list upon click
-//exclude "this" in the filter each time -> removes everything that isn't "this"
 function handleSidebarNode(title) {
   const detailNodes = document.querySelectorAll("#detail div");
   for (let detailElement of detailNodes) {
     if (this.children[0].innerText === detailElement.children[0].innerText) {
+      debugger;
         detailElement.style.display = "inline";
     } else {
         detailElement.style.display = "none";
     }
   }
-  //for loop
-      //check if this === detail things
-        //if true --> display content
-      //
 }
 
-function deletePost () {
-  // let delButton = document.getElementById('delete')
-  var deletes = document.getElementsByClassName('delete-button');
-  console.log
-  for (var i = 0; i < deletes.length; i++) {
-    deletes[i].addEventListener('click', function () {
-      debugger;
-    });
-  }
-}
 
 function removeFromServer () {
-  debugger;
-  // let url = "http://localhost:3000/api/v1/notes"
-  // let id = "#" //somehow find ID
-  // let url_with_id = url + "/" + id
-  //
-  // fetch(url_with_id, {
-  //   method: 'DELETE',
-  //   headers: {
-  //     "Content-type": "application/json"
-  //   }
-  // });
+  let url = "http://localhost:3000/api/v1/notes"
+  let id = "#" //somehow find ID
+  let url_with_id = url + "/" + id
+
+  fetch(url_with_id, {
+    method: 'DELETE',
+    headers: {
+      "Content-type": "application/json"
+    }
+  });
 
 }
 
@@ -80,23 +56,30 @@ function removeFromServer () {
 
 
 
-//find closest stable parent
-// console.log();
-
-
-// sidebar.addEventListener("click", function(e) {
-//     debugger;
-//     this
-// });
-
-
-
 const displayNotes = (notes) => {
-
   notes.forEach( note => {
     sidebar.innerHTML += `<div class="${note.id}"><h2>${note.title}</h2><p>${note.body.slice(0,20)}...</p></div>`
 
-    detail.innerHTML += `<div class="${note.id}"><h2>${note.title}</h2><p>${note.body}</p><button id="edit" class="${note.id} edit-button">Edit</button> <button id="delete" class="${note.id} delete-button">Delete</button></div>`
+    detail.innerHTML += `<div class="${note.id}"><h2>${note.title}</h2><p>${note.body}</p><button id="edit" class="${note.id}">Edit</button> <button id="delete" class="${note.id}">Delete</button></div>`
   })
+}
+
+function editOnServer() {
+
+  let url = "http://localhost:3000/api/v1/notes"
+  let id = "374"
+  let url_with_id = url + "/" + id
+  let editBody = {title: "Brooke's Note", body: "This is brooke's note", user_id: id}
+
+  fetch(`url_with_id`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(editBody)
+  })
+  .then( res => res.json())
+  .then( json => console.log(json));
 
 }
